2c48d9a15b21a01c5911f964ccee493ecea9e9b9
miralda <miraldalexandra15@gmail.com>
2021-11-17 12:07:07 +0100

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">ref</button>
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
@@ -16,6 +18,13 @@ export default {
     return {
       title: "Vue first app",
     };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
   },
 };
 </script>
